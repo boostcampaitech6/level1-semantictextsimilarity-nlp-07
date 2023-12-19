fdedd458b9cbdf4163c85f1fe4698b2c55a6fb8d
9ooDa <dayeonkuhk@gmail.com>
2023-12-19 05:05:52 +0000

    train_path to train_aug_path in train.py random_state(seed) added to sampling in main.py

--- data_preprocessing/main.py
+++ data_preprocessing/main.py
@@ -18,7 +18,7 @@ config = load_config("/data/ephemeral/home/level1-semantictextsimilarity-nlp-07/
 # Down sampling 및 validation data processing
 train_df = down_sample(config, data_name="train_path")
 val_df = pd.read_csv(config["paths"]["dev_path"])
-add_df = train_df.sample(frac=0.15, ignore_index=True)  # 합쳤을 때 대략 20%가 될 수 있게 15%를 가져옴 (original val data는 6% 정도)
+add_df = train_df.sample(frac=0.15, random_state=123, ignore_index=True)  # 합쳤을 때 대략 20%가 될 수 있게 15%를 가져옴 (original val data는 6% 정도)
 
 train_df = train_df[~train_df.index.isin(add_df.index)].reset_index()   # val에 들어 갈 데이터 train에서 빼주기
 val_df = pd.concat([val_df, add_df], ignore_index=True)     # val과 train에서 뗀 데이터 붙이기
